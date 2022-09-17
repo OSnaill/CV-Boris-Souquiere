2e2c84e2b0961a2e2467689a228e062a6c8634c5
Boris Souquière <souquiereb1998@gmail.com>
2022-09-17 10:43:17 +0200

    cv en ligne ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    screens:{
+      'tablet' : '760px',
+      'laptop' : '1020px',
+      'desktop' : '1600px'
+    },
     extend: {},
   },
   plugins: [],
